3234704caf6d1c887a13b7e4f12fabb721f41e8a
kubos777 <chd.jl@gmail.com>
2018-08-15 12:43:11 -0500

    Aprendiendo Swift :alien:

---
+++ Clases/Miercoles15Agosto/MyPlayground.playground/Contents.swift
@@ -0,0 +1,59 @@
+//Agosto 20 de 2018
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var greeting = "Qué onda razita!"
+var endOfApp = "Adiosin :v"
+greeting = "Hola mundo de swift :D"
+
+//let endOfApp = "Adiosin :v" <---Marca error
+//Var permite la mutabilidad y let es para constantes
+//otherStr = "fsafsa"
+
+endOfApp = "Adios bai! 😱"
+
+var 😁 = "Sonriendo"
+
+let perro = "🐶"
+//Concatenación
+var perroSonriente = perro + " " + 😁
+
+
+let firstName = "Jorge Chávez" //String
+let city = "CDMX"
+//Interpolación
+let welcomeString = "Hola \"\(firstName)\", \n bienvenido a \(city)"
+//Tipos de datos, se asignan mediante la inferencia
+let number = 99 //Int
+let decimal = 5.5 //Double
+let boolean = false //Bool
+
+//Anotación de tipo
+
+let lastName:String
+let otherNumber:Int
+let decimalNumber:Double
+let otherBoolean:Bool
+
+lastName = "Chávez"
+otherNumber = 32
+decimalNumber = 3.14
+otherBoolean = false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
