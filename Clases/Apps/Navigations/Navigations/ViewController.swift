ca1d1bd20f4fcdb1f8c48760ce9d6e9a2d482ac6
Usuario invitado <Guest@MacBook-9.local>
2018-09-26 12:24:58 -0500

    Ejercicio de segues, textInput. :alien:

--- Clases/Apps/Navigations/Navigations/ViewController.swift
+++ Clases/Apps/Navigations/Navigations/ViewController.swift
@@ -19,10 +19,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Para regresar al menú de la forma correcta
     @IBAction func unwindToFirst(unwindSegue: UIStoryboardSegue){
         
         }
 
-
+    @IBOutlet weak var textField: UITextField!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //print("Se ejecuto el segue en red")
+        segue.destination.navigationItem.title = textField.text
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
