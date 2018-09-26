376783dff463208084065cb15c1aed4c9a3c58cc
kubos777 <chd.jl@hotmail.com>
2018-09-26 13:02:12 -0500

    :snake: Agrego ejercicio de tab view controller

---
+++ Clases/Apps/Controls/Controls/OrangeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  OrangeViewController.swift
+//  Controls
+//
+//  Created by Usuario invitado on 26/9/18.
+//  Copyright © 2018 kubos777. All rights reserved.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tabBarItem.badgeValue = "🐥"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Clases/Apps/Controls/Controls/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Controls
+//
+//  Created by Usuario invitado on 26/9/18.
+//  Copyright © 2018 kubos777. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tabBarItem.badgeValue = "👽"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
