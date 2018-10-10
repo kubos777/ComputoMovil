1ab3fa7071ea9cd2270f1bdb7cbe2233e9687a17
Usuario invitado <Guest@MacBook-8.local>
2018-10-10 12:54:55 -0500

    Agrego segundo ejercicio no terminado, problemas con los constraints

---
+++ Clases/Apps/personalityQuiz/personalityQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  personalityQuiz
+//
+//  Created by Usuario invitado on 10/10/18.
+//  Copyright © 2018 kubos777. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
